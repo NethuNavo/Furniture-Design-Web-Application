7b73263450bbf8c1a0eef0c3c7e449f9d909e0f1
NethuNavo <nethunavo24@gmail.com>
2026-03-18 18:42:10 +0530

    Clear branch 10953046-Jayamani for fresh start

--- src/app/login/page.tsx
+++
@@ -1,9 +0,0 @@
-import { AuthForm } from "@/components/auth/AuthForm";
-
-export default function LoginPage() {
-  return (
-    <main className="min-h-screen bg-stone-100 text-charcoal">
-      <AuthForm mode="login" />
-    </main>
-  );
-}
--- src/app/signup/page.tsx
+++
@@ -1,9 +0,0 @@
-import { AuthForm } from "@/components/auth/AuthForm";
-
-export default function SignupPage() {
-  return (
-    <main className="min-h-screen bg-stone-100 text-charcoal">
-      <AuthForm mode="signup" />
-    </main>
-  );
-}
